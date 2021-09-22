7160217fa860b199d1377a68fc997f8c35819b8f
Anycart <sheyngis03@yandex.ru>
2021-09-22 19:31:03 +0300

    z/2 z/3 PZ 1

---
+++ ПЗ 1/Z2/Z2/Z2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a, b, c, rez;
+	cout << "Введите число:" << endl;
+	cin >> rez;
+	a = rez % 10;
+	b = rez / 10 % 10;
+	c = rez / 100;
+	cout << (a + b + c) / 3.0 << endl;
+	return 0;
+}
---
+++ ПЗ 1/Z3/Z3/Z3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите номер месяца" << endl;
+	int month;
+	cin >> month;
+	switch (month)
+	{
+	case 12: cout << "Декабрь"; break;
+	case 1:cout << "Январь"; break;
+	case 2: cout << "Февраль"; break;
+	case 3: cout << "Март"; break;
+	case 4: cout << "Апрель"; break;
+	case 5: cout << "Май"; break;
+	case 6: cout << "Июнь"; break;
+	case 7: cout << "Июль"; break;
+	case 8: cout << "Август"; break;
+	case 9: cout << "Сентябрь"; break;
+	case 10: cout << "Октябрь"; break;
+	case 11: cout << "Ноябрь"; break;
+	default: cout << "Такого месяца нет" << endl;
+	}
+}
