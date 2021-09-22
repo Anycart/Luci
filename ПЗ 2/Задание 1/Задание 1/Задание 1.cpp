35a9ea786b534959f45a593b60124d784dadae03
Anycart <sheyngis03@yandex.ru>
2021-09-22 19:01:50 +0300

    Задание 1 пз 2

---
+++ ПЗ 2/Задание 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите размеры прямоугольника" << endl;
+	int a, b, rez1, rez2;
+	cin >> a;
+	cin >> b;
+	rez1 = a * b;
+	rez2 = 2 * (a + b);
+	cout << "Площадь прямоугольника = " << rez1 << endl;
+	cout << "Периметр прямоугольника = " << rez2 << endl;
+}
