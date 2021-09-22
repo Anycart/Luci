6102ec6d9b78c6bd52c9b32913e655d54d16b0b8
Anycart <sheyngis03@yandex.ru>
2021-09-22 16:53:46 +0300

    Задание 1

---
+++ Zadanie 4/Zadanie 4/Zadanie 4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите номер месяца" << endl;
+	int month;
+	cin >> month;
+	switch (month)
+	{
+	case 12:
+	case 1:
+	case 2: cout << "Зима"; break;
+	case 3:
+	case 4:
+	case 5: cout << "Весна"; break;
+	case 6:
+	case 7:
+	case 8: cout << "Лето"; break;
+	case 9:
+	case 10:
+	case 11: cout << "Осень"; break;
+	default: cout << "Такого месяца нет" << endl;
+	}
+}
