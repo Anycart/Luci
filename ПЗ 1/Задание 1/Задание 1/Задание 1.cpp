68892d004071f32b3229b3aec34e5ec2474d26ca
Anycart <sheyngis03@yandex.ru>
2021-09-22 19:16:02 +0300

    Задание 1, 4 пз 1

---
+++ ПЗ 1/Задание 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int a;
+	cout << "Введите число " << endl;
+	cin >> a;
+	if (a % 2 == 0) cout << "четное число" << endl;
+	else cout << "Нечетное число" << endl;
+}
